3d55604669ba35de552128c7585de7a54db04297
Lorenzo Lancia <44911690+LorenzoLancia@users.noreply.github.com>
2024-03-17 12:52:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ let bot;
 
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 }
 else {
   bot = new Bot(token, { polling: true });
