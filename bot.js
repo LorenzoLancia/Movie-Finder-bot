e86da64da394d2a125a3af89584101baf0a9f022
Lorenzo Lancia <44911690+LorenzoLancia@users.noreply.github.com>
2024-03-31 18:40:05 +0200

    Update bot.js
    Heroku to Vercel

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ let bot;
 
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot.setWebHook(process.env.VERCEL_URL + bot.token);
 }
 else {
   bot = new Bot(token, { polling: true });
